606e5e198ed86b72a0af731721b68fdd185b64c8
Anya Vakharia <vakha21@wfu.edu>
2021-12-03 16:47:47 -0500

    updated.

--- src/WorkingCheckers.java
+++ src/WorkingCheckers.java
@@ -524,68 +524,60 @@ public class WorkingCheckers extends JPanel {
                 }
             }  //
 
-
-            /**
-             * This is called by the two previous methods to check whether the
-             * player can legally jump from (r1,c1) to (r3,c3).  It is assumed
-             * that the player has a piece at (r1,c1), that (r3,c3) is a position
-             * that is 2 rows and 2 columns distant from (r1,c1) and that
-             * (r2,c2) is the square between (r1,c1) and (r3,c3).
+            /*check whether the player can make a legal jump from (r1, c1) to (r3, c3).
+            Assume that there is already a piece at (r1, c1).
+            Assume that (r3,c3) is a position that is 2 rows and 2 columns distant from (r1,c1).
+            (r2,c2) will be the square in between (r1,c1) and (r3,c3)
              */
             private boolean canJump(int player, int r1, int c1, int r2, int c2, int r3, int c3) {
 
                 if (r3 < 0 || r3 >= 8 || c3 < 0 || c3 >= 8)
-                    return false;  // (r3,c3) is off the board.
+                    return false;
 
                 if (board[r3][c3] != EMPTY)
-                    return false;  // (r3,c3) already contains a piece.
+                    return false;  // (r3,c3) already has a piece.
 
                 if (player == RED) {
                     if (board[r1][c1] == RED && r3 > r1)
-                        return false;  // Regular red piece can only move up.
+                        return false;  // Red pieces can only move upwards.
                     if (board[r2][c2] != BLACK && board[r2][c2] != BLACK_KING)
-                        return false;  // There is no black piece to jump.
-                    return true;  // The jump is legal.
+                        return false;  // no black pieces to jump.
+                    return true;  // jump is legal.
                 } else {
                     if (board[r1][c1] == BLACK && r3 < r1)
-                        return false;  // Regular black piece can only move downn.
+                        return false;  // Black piece can only move downwards.
                     if (board[r2][c2] != RED && board[r2][c2] != RED_KING)
-                        return false;  // There is no red piece to jump.
-                    return true;  // The jump is legal.
+                        return false;  // no red pieces to jump.
+                    return true;  // jump is legal.
                 }
 
-            }  // end canJump()
+            }
 
 
-            /**
-             * This is called by the getLegalMoves() method to determine whether
-             * the player can legally move from (r1,c1) to (r2,c2).  It is
-             * assumed that (r1,r2) contains one of the player's pieces and
-             * that (r2,c2) is a neighboring square.
+            /*
+             * getLegalMoves() method calls this in order to determine whether a player can legally move
+             from (r1,c1) to (r2,c2).
+             Assume that (r1,r2) contains a player's piece and (r2,c2) is a neighboring square.
              */
+
             private boolean canMove(int player, int r1, int c1, int r2, int c2) {
 
                 if (r2 < 0 || r2 >= 8 || c2 < 0 || c2 >= 8)
-                    return false;  // (r2,c2) is off the board.
+                    return false;
 
                 if (board[r2][c2] != EMPTY)
                     return false;  // (r2,c2) already contains a piece.
 
                 if (player == RED) {
                     if (board[r1][c1] == RED && r2 > r1)
-                        return false;  // Regular red piece can only move down.
-                    return true;  // The move is legal.
+                        return false;  // Red pieces can only move downwards.
+                    return true;  //legal move.
                 } else {
                     if (board[r1][c1] == BLACK && r2 < r1)
-                        return false;  // Regular black piece can only move up.
-                    return true;  // The move is legal.
+                        return false;  // Black pieces can only move upwards.
+                    return true;  // legal move.
                 }
-
-            }  // end canMove()
-
-
-        } // end class CheckersData
-
-
-    } // end class Checkers
+            }
+        }
+    }
 }
